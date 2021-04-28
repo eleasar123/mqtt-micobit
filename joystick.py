00028dca0a3974d33aa25ce4becfb3426a113f20
2ndyrGroupB <2ndyrGroupB@pn.local>
2021-04-28 20:50:18 +0800

    Added all

--- joystick.py
+++ joystick.py
@@ -8,6 +8,7 @@ def on_connect(client, userdata, flags, rc):
         display.show(Image.YES)
 
 def on_message(client, userdata, msg):
+    print(script)
     if msg.payload.decode() == "north":
         display.show(Image.ARROW_N)
     if msg.payload.decode() == "northeast":
@@ -31,6 +32,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("wss://test.mosquitto.org:8081/mqtt", 1883, 60)
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
 
 client.loop_forever()
